0392aa1c162af5f618d0894f0bb6e1ac4249b4ac
Rosanna Zhang <rosannazhang92@gmail.com>
2022-12-22 23:39:54 -0800

    Refactor loopLighthouse logic with new function

--- W1-loopylighthouse.js
+++ W1-loopylighthouse.js
@@ -1,18 +1,39 @@
-/* Function that takes in three parameters and prints accordingly
-*/
+/* Function that takes in three parameters and prints accordingly */
+
+// ORIGINAL LOGIC
+// const loopyLighthouse = function (range, multiples, words) {
+//   for (let x = range[0]; x < range[1] + 1; x++) {
+//     if (x % multiples[0] === 0 && x % multiples[1] === 0) {
+//       console.log(words[0] + words[1]);
+//     } else if (x % multiples[0] === 0) {
+//       console.log(words[0]);
+//     } else if (x % multiples[1] === 0) {
+//       console.log(words[1]);
+//     } else {
+//       console.log(x);
+//     }
+//   }
+// };
+
+// AFTER REFACTOR
 
 const loopyLighthouse = function (range, multiples, words) {
   for (let x = range[0]; x < range[1] + 1; x++) {
-    if (x % multiples[0] === 0 && x % multiples[1] === 0) {
-      console.log(words[0] + words[1]);
-    } else if (x % multiples[0] === 0) {
-      console.log(words[0]);
-    } else if (x % multiples[1] === 0) {
-      console.log(words[1]);
-    } else {
-      console.log(x);
+    let output = "";
+    output += checkMultiples(x, multiples[0], words[0])
+    output += checkMultiples(x, multiples[1], words[1])
+    console.log(output == "" ? x : output);
     }
+  
   }
-};
+
+const checkMultiples = function(x, multiple, word) {
+  if (x%multiple === 0) {
+    return word;
+  }
+  else {
+    return "";
+  }
+}
 
 loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
