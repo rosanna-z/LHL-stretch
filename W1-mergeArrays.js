1d90ae9f781797e732b632f8d90bdec42a2d4d12
Rosanna Zhang <rosannazhang92@gmail.com>
2022-12-22 23:46:09 -0800

    Create new file with merge function without logic

---
+++ W1-mergeArrays.js
@@ -0,0 +1,9 @@
+// when given two sorted arrays, returns a sorted array of the numbers from both lists.
+
+const merge = function() {
+
+}
+
+merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]); // expected: [ 1, 2, 3, 4, 4, 5, 6 ]
+merge([ 4 ], [ 2, 5, 8 ]);	//expected: [ 2, 4, 5, 8 ]
+merge([ 1, 2, 6 ], []); // expected: [ 1, 2, 6 ]
